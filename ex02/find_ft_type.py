196d65baad7c3033db9424cf22fa3578c5dce703
Azbk42 <mauduit.pro@outlook.com>
2024-12-11 15:56:28 +0100

    push

---
+++ ex02/find_ft_type.py
@@ -0,0 +1,18 @@
+def all_thing_is_obj(object: any) -> int:
+    type_object = type(object)
+    
+    if type_object is list:
+        print("List : " + str(type_object))
+    elif type_object is tuple:
+        print("Tuple : " + str(type_object)) 
+    elif type_object is set:
+        print("Set : " + str(type_object)) 
+    elif type_object is dict:
+        print("Dict : " + str(type_object)) 
+    elif type_object is str:
+        print(f'{object}  is in the kitchen : ' + str(type_object))
+    else:
+        print("Type not found")
+    
+    return 42
+
